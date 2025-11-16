5c564fdc54d0eac7518772050e87b2345a8714f4
Gerome <geromeemmanuel.param@cvsu.edu.ph>
2025-11-16 18:52:37 +0800

    fixed the nav bar for smartphones. FINALLY

--- Dashboard/dashboard.js
+++ Dashboard/dashboard.js
@@ -1,3 +1,4 @@
 function show(){
-    document.querySelector('.navLink').classList.toggle('show')
+    document.querySelector('.navLink').classList.toggle('show');
+    document.querySelector('#overlay').classList.toggle('active');
 }
